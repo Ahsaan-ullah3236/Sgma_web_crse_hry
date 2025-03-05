4a6d12108adf251f93a76bb5abb644cd0f74be24
Ahsaan-ullah3236 <158459990+Ahsaan-ullah3236@users.noreply.github.com>
2025-03-05 12:28:43 +0500

    Add initial HTML and JavaScript files for hacking simulator project for the practice

---
+++ 78Class/Practice.js
@@ -0,0 +1,73 @@
+// first 
+// let Firsth1= document.getElementsByClassName("h1")[0]
+// console.log(Firsth1);
+
+// async function myfunction() {
+//     return new Promise((resolve, reject) => {
+//        setTimeout(() => {
+//         document.body.innerHTML = Firsth1.innerHTML;
+//        }, );
+//         resolve();
+//     })
+// }
+// async function mian(){
+//     let
+// }
+// myfunction()
+
+// second 
+// let headings = document.querySelectorAll("h1");
+
+// function blinkElement(element, duration) {
+//     return new Promise((resolve) => {
+//         let visible = true;
+//         let interval = setInterval(() => {
+//             element.style.visibility = visible ? "hidden" : "visible";
+//             visible = !visible;
+//         }, 500); // Blink every 500ms
+
+//         setTimeout(() => {
+//             clearInterval(interval);
+//             element.style.visibility = "visible"; // Ensure visibility
+//             resolve();
+//         }, duration);
+//     });
+// }
+
+// async function startBlinking() {
+//     for (let heading of headings) {
+//         await blinkElement(heading, 2000); // Each heading blinks for 2 seconds
+//     }
+// }
+
+// startBlinking();
+
+// third 
+
+const additem = async(item) => {
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div)
+}
+const randomDelay = ()=>{
+    return new Promise((resolve, reject) => {
+        let timeout = 1+6 *Math.random();
+        setTimeout(() => {
+            resolve();
+        }, timeout * 2000);
+    })
+}
+let text = ["Initilizing Hackign...",
+    "Reading your Files...",
+    "Password Files Detected...",
+    " Sendig all password and the personal files to the server...",]
+    const showMessages = async () => {
+    for (const item of text) {
+        await additem(item);
+    }
+};
+
+showMessages();
+
+   
